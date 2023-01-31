8e7edaaa153fd128fb064b519ec2bed0e222dc57
Franco Goyena <gfrancomariano@yahoo.com.ar>
2023-01-31 14:55:03 -0300

    layout

---
+++ SLNClinica/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace SLNClinica.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Nombre = "Bienvenido al sistema de Medicos";
+            ViewBag.Fecha = DateTime.Now.ToString();
+            return View();
+        }
+    }
+}
